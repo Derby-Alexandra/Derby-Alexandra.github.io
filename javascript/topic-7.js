f2a092e7d74c07b6f2ed3b5af77329101b6d2e5a
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2019-06-17 17:06:13 -0700

    Update topic-7.js

--- javascript/topic-7.js
+++ javascript/topic-7.js
@@ -1,19 +1,3 @@
-// toggle between 3 different images with a mouse click
-var imagechanger = 'pizza';
-function changeimage() {
-    var image = document.getElementById('pizzaburger');
-    if (imagechanger == 'pizza') {
-        image.src = '../images/pizzaimage.png';
-        imagechanger = 'burger';
-    } else if (imagechanger == 'burger') {
-        image.src = '../images/burgerimage.png';
-        imagechanger = 'hotdog';
-      } else if (imagechanger == 'hotdog') {
-        image.src = '../images/hotdogimage.png';
-        imagechanger = 'pizza';
-    }
-}
-
 // change the color, size, and font of the first header
 function changefirstheader() {
     document.getElementById("firstheader").classList.toggle("change1");
@@ -35,4 +19,20 @@ function changeparagraphs(){
     for(i=0; i<headerchange1.length; i++) {
         headerchange1[i].style.color = '#5E6973';
         }   
+}
+
+// toggle between 3 different images with a mouse click
+var imagechanger = 'pizza';
+function changeimage() {
+    var image = document.getElementById('pizzaburger');
+    if (imagechanger == 'pizza') {
+        image.src = '../images/pizzaimage.png';
+        imagechanger = 'burger';
+    } else if (imagechanger == 'burger') {
+        image.src = '../images/burgerimage.png';
+        imagechanger = 'hotdog';
+      } else if (imagechanger == 'hotdog') {
+        image.src = '../images/hotdogimage.png';
+        imagechanger = 'pizza';
+    }
 }

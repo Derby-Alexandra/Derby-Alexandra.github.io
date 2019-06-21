d25d09c3b1de8891b6d7b7d885d60c56575c4bb3
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2019-06-21 14:39:24 -0700

    Update topic-6.js

--- javascript/topic-6.js
+++ javascript/topic-6.js
@@ -1,36 +1,42 @@
 //Replace all the current headers and paragraphs with new ones using JS
 function addelements() {
+    //paragraph1
     var parent = document.getElementById("div1");
     var child = document.getElementById("p1");
     var para = document.createElement("p");
     var node = document.createTextNode("New paragraph 1 created with JavaScript");
     para.appendChild(node);
     parent.replaceChild(para,child);
-
+    
+    //paragraph2
     var child2 = document.getElementById("p2");
     var para2 = document.createElement("p");
     var node2 = document.createTextNode("New paragraph 2 created with JavaScript");
     para2.appendChild(node2);
     parent.replaceChild(para2,child2);
 
+    //paragraph3
     var child3 = document.getElementById("p3");
     var para3 = document.createElement("p");
     var node3 = document.createTextNode("New paragraph 3 created with JavaScript");
     para3.appendChild(node3);
     parent.replaceChild(para3,child3);
 
+    //paragraph4
     var child4 = document.getElementById("p4");
     var para4 = document.createElement("p");
     var node4 = document.createTextNode("New paragraph 4 created with JavaScript");
     para4.appendChild(node4);
     parent.replaceChild(para4,child4);
 
+    //header1
     var headerchild1 = document.getElementById("firsth4");
     var header1 = document.createElement("h4");
     var headernode1 = document.createTextNode("New Header 1 Created with JavaScript");
     header1.appendChild(headernode1);
     parent.replaceChild(header1,headerchild1);
-
+    
+    //header2
     var headerchild2 = document.getElementById("secondh4");
     var header2 = document.createElement("h4");
     var headernode2 = document.createTextNode("New Header 2 Created with JavaScript");

138ebb8c677754bfeef5e4a7a691161fe33b2330
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2019-06-24 12:32:05 -0700

    topic 8 updates

---
+++ javascript/topic-8.js
@@ -0,0 +1,25 @@
+function qmousedown1() {
+    document.getElementById("q1").style.backgroundColor = "#BDFFFD";
+}
+
+function submit1() {
+    var firstname = document.getElementById("q1").value;
+    if (firstname == 8) {
+        document.getElementById("q1").style.backgroundColor = "green";
+    } else {
+        document.getElementById("q1").style.backgroundColor = "red";
+    }   
+}
+
+
+function qmousedown2() {
+    document.getElementById("q2").style.backgroundColor = "#BDFFFD";
+}
+
+function qmousedown3() {
+    document.getElementById("q3").style.backgroundColor = "#BDFFFD";
+}
+
+function qmousedown4() {
+    document.getElementById("q4").style.backgroundColor = "#BDFFFD";
+}

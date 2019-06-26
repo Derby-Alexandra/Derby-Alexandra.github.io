153708817a64afcbd4015a7462e7022ae58abf37
alexandra-derby <35322727+alexandra-derby@users.noreply.github.com>
2019-06-26 16:25:30 -0700

    topic 10 updates

--- javascript/topic-10.js
+++ javascript/topic-10.js
@@ -3,9 +3,9 @@ function drawcanvas() {
     var ctx = canvas.getContext("2d");
     
     ctx.fillStyle = "#FF8852";
-    ctx.fillRect(0,0,150,75);
+    ctx.fillRect(100,0,150,75);
     ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
+    ctx.fillRect(100, 100, 50, 50);
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     ctx.fillRect(275, -5, 125, 125);
     ctx.fillRect(200, 200, 300, 200);
